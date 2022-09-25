abd19f1f8f8f1c0d88cb6f6537e9431113cfdd98
Eslam Ebrahim <e.ebrahim.1994@gmail.com>
2022-09-25 21:13:57 +0200

    initial table design  for library books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_25_185343) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_25_190801) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_25_185343) do
     t.index ["uid", "provider"], name: "index_admins_on_uid_and_provider", unique: true
   end
 
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.bigint "section_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["section_id"], name: "index_books_on_section_id"
+  end
+
   create_table "sections", force: :cascade do |t|
     t.string "name"
     t.string "from_bookcase_code"
@@ -45,4 +54,5 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_25_185343) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "books", "sections"
 end
