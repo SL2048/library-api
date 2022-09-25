06a3d7272d931a1a4f1a078c39ac14d9d582f298
Eslam Ebrahim <e.ebrahim.1994@gmail.com>
2022-09-25 21:35:26 +0200

    creating join table between books and authors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_25_192825) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_25_193350) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -41,6 +41,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_25_192825) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.bigint "author_id", null: false
+    t.bigint "book_id", null: false
+    t.index ["author_id", "book_id"], name: "index_authors_books_on_author_id_and_book_id"
+    t.index ["book_id", "author_id"], name: "index_authors_books_on_book_id_and_author_id"
   end
 
   create_table "books", force: :cascade do |t|
