84c3be3333f0aede472b1c00dfa30a0231c7bc6a
Eslam Ebrahim <e.ebrahim.1994@gmail.com>
2022-09-26 11:06:06 +0200

    running model specs for Inventory model and handling errors

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,3 +1,8 @@
 class Inventory < ApplicationRecord
   belongs_to :book
+
+  validates_presence_of :code
+  validates_length_of :code, is: 24
+
+  enum status: { available: 0, borrowed: 1, stashed: 2 }
 end
