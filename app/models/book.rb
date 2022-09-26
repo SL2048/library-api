b390782cde41f000d5254fae1edb0a96fca99cb8
Eslam Ebrahim <e.ebrahim.1994@gmail.com>
2022-09-26 11:00:37 +0200

    running model specs for Book model and handling errors

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :section
+  has_many :inventories
+  has_and_belongs_to_many :authors
+  has_and_belongs_to_many :genres
+
+  validates_presence_of :title, :description
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe Book, type: model do
+RSpec.describe Book, type: :model do
   context 'associations' do
     it { is_expected.to belong_to(:section) }
     it { is_expected.to have_many(:inventories) } 
